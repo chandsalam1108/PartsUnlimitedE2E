5867f0c253bb48c4993a72158dfb9ec3fe90f3cc
Abdul Salam Chand <chandsalam1108@gmail.com>
2020-12-17 08:26:36 +0000

    This is a dummy checkin

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/Category.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    //Dummy Comments...
     public class Category
     {
         public int CategoryId { get; set; }
